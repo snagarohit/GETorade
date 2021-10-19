3f76e5a6e46831857d966566d59e6dd4374a7159
Naga Samineni <snagarohit@gmail.com>
2021-10-19 00:22:48 +0000

    Updating home page gif.

--- routes/root/index.py
+++ routes/root/index.py
@@ -1,4 +1,4 @@
 from src.common.template import centeredHtml
 
 def main():
-  return centeredHtml.render('<img src="https://i.pinimg.com/originals/3a/7e/43/3a7e434cc7cde1b176234cc12b3a5bc8.gif"/>')
+  return centeredHtml.render('<img src="https://c.tenor.com/RvMZMiTblfQAAAAC/oh-hey-oh-hey-there.gif"/>')
